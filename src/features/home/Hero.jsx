62ad796c91c7b31469ae2d5d14e79eee6287f53e
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-06-11 06:17:49 +0200

    worked on the homepage

--- src/features/home/Hero.jsx
+++ src/features/home/Hero.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Hero() {
   return (
     <div className={`h-screen max-xl:h-3/4 w-full bg-[url('/group_photo.jpeg')] bg-cover bg-no-repeat bg-center`}>
-        <div className="h-full p-8 max-md:pt-40 bg-[rgba(31,31,31,0.8)] flex flex-col justify-center items-center text-center">
+        <div className="h-full px-8 max-sm:px-4 py-14 max-md:pt-40 bg-[rgba(31,31,31,0.8)] flex flex-col justify-center items-center text-center">
             <h1 className="text-5xl max-sm:text-4xl text-[#FFF] font-semibold max-md:leading-normal">Nurturing Leaders of Tomorrow</h1>
             <p className="text-xl text-[#c8c8c8] w-[70%] max-md:w-[100%] leading-relaxed mt-6">Our supportive community, state-of-the-art facilities, and innovative programs provide the ideal environment 
             for students to flourish and reach their full potential.</p>
