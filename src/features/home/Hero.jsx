cd8a45e34ec7b95cbb368033de7a63b41aacb877
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-06-11 06:22:58 +0200

    worked on the homepage

--- src/features/home/Hero.jsx
+++ src/features/home/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
             <h1 className="text-5xl max-sm:text-4xl text-[#FFF] font-semibold max-md:leading-normal">Nurturing Leaders of Tomorrow</h1>
             <p className="text-xl text-[#c8c8c8] w-[70%] max-md:w-[100%] leading-relaxed mt-6">Our supportive community, state-of-the-art facilities, and innovative programs provide the ideal environment 
             for students to flourish and reach their full potential.</p>
-            <Link to={"/about_us"} className="text-[#fff] bg-blue-500 hover:bg-[#1D4ED8] text-[1.1rem] transition-all shadow-2xl p-3 px-8 mt-10 rounded-[10px]">Discover more</Link>
+            <Link to="/about_us" className="text-[#1D4ED8] bg-white border border-blue-500 hover:bg-[#1D4ED8] hover:text-[#fff] text-[1.1rem] transition-all shadow-2xl p-3 px-8 mt-10 rounded-[10px]">Discover more</Link>
         </div>
     </div>
   )
