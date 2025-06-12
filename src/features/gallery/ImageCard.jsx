d4fe61a115d1cd771698e22c90bcabbd539da516
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-06-12 05:55:19 +0200

    worked on the contact and gallery pages

--- src/features/gallery/ImageCard.jsx
+++ src/features/gallery/ImageCard.jsx
@@ -1,5 +1,5 @@
 export default function ImageCard({img}) {
   return (
-    <img src={img} alt="image" className="brightness-75 rounded-md" width={300} />
+    <img src={img} alt="image" className="brightness-75 rounded-md" width={300} loading="lazy" />
   )
 }
