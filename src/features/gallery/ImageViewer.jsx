bf173a2f14b1601101033ad269827a44af7abc84
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-06-13 07:20:19 +0200

    Created the image viewer

--- src/features/gallery/ImageViewer.jsx
+++ src/features/gallery/ImageViewer.jsx
@@ -4,7 +4,7 @@ import { LuChevronLeft, LuChevronRight } from 'react-icons/lu'
 export default function ImageViewer({currentImg}) {
   return (
     <div className='relative'>
-      <img src={currentImg.URI} alt="image" className='h-[500px] min-w-[500px] shadow-2xl rounded-[10px] max-sm:w-[80%] max-sm:h-[400px]'/>
+      <img src={currentImg.URI} alt="image" className='h-[500px] min-w-[500px] shadow-2xl rounded-[10px] max-sm:w-[80%] max-sm:h-[400px] object-cover'/>
     </div>
   )
 }
